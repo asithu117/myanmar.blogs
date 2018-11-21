0626faf9c719b718d32c285200489525453fceea
asithu117 <myatthiriwinhtut@gmail.com>
2018-11-21 09:10:13 +0630

    Add View   -signin,signup

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/signup', function(req, res, next) {
+  res.render('commons/signup', { title: 'Sign Up' });
+});
+router.get('/signin', function(req, res, next) {
+  res.render('commons/signin', { title: 'Sign In' });
+});
 module.exports = router;
